d3222a1159294e2a534cf9a3ec9e2fad6a7dea4c
Devansh Lachake <workdev30500@gmail.com>
2025-07-18 02:07:49 +0000

    trying again

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -24,10 +24,9 @@ public class SocialMediaController {
      * suite must receive a Javalin object from this method.
      * @return a Javalin app object which defines the behavior of the Javalin controller.
      */
-    // Testing if now commit is working
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        
+
         app.post("/register", this::registerHandler);
         app.post("/login", this::loginHandler);
 
